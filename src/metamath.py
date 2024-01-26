951972a9cd3192377b8683a2c35d8a1eb3a85c4b
TheG3ntleman <sriabhirath21@gmail.com>
2024-01-26 21:51:12 +0530

    Made the files.

---
+++ src/metamath.py
@@ -0,0 +1,7 @@
+def validate_proof(proof_str):
+    """
+        Input: Proof in string format
+        Output: Dictionary of the following information:
+            a. 1 for correct / 0 for wrong
+            b. Statement Number
+    """
---
+++ src/models.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+
+class Instructor(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        pass
